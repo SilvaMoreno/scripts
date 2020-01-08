68412660b0d441e870f7c8a19334a79227cfb0e3
Elton Moreno <elton989@hotmail.com>
2020-01-08 22:47:03 -0100

    mysql added

--- docker_containers.sh
+++ docker_containers.sh
@@ -1,2 +1,5 @@
+// Postgres
 sudo docker run --name my-postgres --network=postgres-network -e "POSTGRES_PASSWORD=postgres" -p 5432:5432 -v /home/silva/Documents/dbs/postgres:/var/lib/postgresql/data -d postgres
 
+// MySql
+docker run --name my-mysql -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mysql
